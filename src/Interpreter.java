989bd325e8da19763c6a58573d3587283a13f1cb
JoshuaCleverley <joshuacleverley@protonmail.com>
2022-10-03 20:14:26 +0100

    Mutable global variables

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -109,6 +109,13 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         return environment.get(expr.name);
     }
 
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Object value = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return true;
+    }
+
     private String stringify(Object object) {
         if (object == null) return "null";
         if (object instanceof Double) {
