8764c7b2fff37296e6379fd306e068a138816e1f
JoshuaCleverley <joshuacleverley@protonmail.com>
2022-10-04 19:12:06 +0100

    Add static analysis using Resolver

--- src/Environment.java
+++ src/Environment.java
@@ -12,18 +12,29 @@ public class Environment {
     Environment(Environment enclosing) {
         this.enclosing = enclosing;
     }
+
     void define(String name, Object value) {
         values.put(name, value);
     }
 
-    Object get(Token name) {
-        if (values.containsKey(name.lexeme)) {
-            return values.get(name.lexeme);
+    Environment ancestor(int distance) {
+        Environment environment = this;
+        for (int i = 0; i < distance; i++) {
+            assert environment != null;
+            environment = environment.enclosing;
         }
+        return environment;
+    }
 
+    Object get(Token name) {
+        if (values.containsKey(name.lexeme)) return values.get(name.lexeme);
         if (enclosing != null) return enclosing.get(name);
 
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'");
+    }
+
+    Object getAt(int distance, String name) {
+        return ancestor(distance).values.get(name);
     }
 
     void assign(Token name, Object value) {
@@ -36,5 +47,9 @@ public class Environment {
             return;
         }
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'");
+    }
+
+    void assignAt(int distance, Token name, Object value) {
+        ancestor(distance).values.put(name.lexeme, value);
     }
 }
