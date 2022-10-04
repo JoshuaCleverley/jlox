b1b0d7bc049af1b95f15e92b12922de7e2f6296a
JoshuaCleverley <joshuacleverley@protonmail.com>
2022-10-04 19:39:01 +0100

    Class declaration

--- src/Parser.java
+++ src/Parser.java
@@ -23,6 +23,7 @@ public class Parser {
 
     private Stmt declaration() {
         try {
+            if (match(TokenType.CLASS)) return classDeclaration();
             if (match(TokenType.FN)) return function("function");
             if (match(TokenType.LET)) return letDeclaration();
             return statement();
@@ -30,6 +31,20 @@ public class Parser {
             synchronize();
             return null;
         }
+    }
+
+    private Stmt classDeclaration() {
+        Token name = consume(TokenType.IDENTIFIER, "Expect class name");
+        consume(TokenType.LEFT_BRACE, "Expect '{' before class body");
+
+        List<Stmt.Function> methods = new ArrayList<>();
+        while (!check(TokenType.RIGHT_BRACE) && !isAtEnd()) {
+            methods.add(function("method"));
+        }
+
+        consume(TokenType.RIGHT_BRACE, "Expect '}' after class body");
+
+        return new Stmt.Class(name, methods);
     }
 
     private Stmt.Function function(String kind) {
